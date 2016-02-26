ab9e766f4035e519e4d15d8ddfc6091517adbd32
Niklas Ojala <niklas.ojala@gmail.com>
2016-02-26 11:14:59 +0200

    Statistics gender pie

--- statistics/statistics.js
+++ statistics/statistics.js
@@ -1,8 +1,51 @@
-regApp.controller('statisticsGender', function () {
-  this.labels = ["Man", "Kvinna", "Odef."];
-  this.data = [4000, 1200, 100];
+
+regApp.controller('statisticsGender', function ($http, globalParams) {
+    this.labels = [];
+    this.data = [];    
+    this.show = true;    
+    var labels = [];
+    var data = [];
+    var show = true;
+    
+    var lang = [];
+    lang["FEMALE"] = "Kvinna";
+    lang["MALE"] = "Man";
+    lang[null] = "Odefinierad";
+
+    var query = {
+        "gs" : {
+            "service": "entry/statistics",
+            "arguments": {
+                "select": "gender",
+                "filter": {
+                    "class": "PERSON",
+                    "registry": globalParams.get('user').registry
+                }
+            }
+        }
+    };
+    
+    $http
+        .post("http://api.registry.huset.fi/", 
+            query)
+        .then(function(response) {
+            angular.forEach(response.data.gs.data, function (value, key) {
+                labels.push(lang[value.gender]);    
+                data.push(value.found);
+            });
+            this.labels = labels;
+            this.data = data;
+        }).catch(function(response) {
+            show = false;
+        });
+
+    this.labels = labels;
+    this.data = data;
+    this.show = show;
 });
-regApp.directive('statisticsGender', function () {
+
+
+regApp.directive('statisticsGender', function ($http) {
     return {
         restrict: 'E',
         templateUrl: 'statistics/templates/statisticsGender.html'
@@ -11,6 +54,8 @@ regApp.directive('statisticsGender', function () {
 
 
 regApp.controller('statisticsAge', function () {
+    
+    
     this.labels = ["1900", "1910", "1920", "1930", "1940", "1950", "1960"];
     this.data = [
         [28, 48, 68, 98, 120, 27, 10]
