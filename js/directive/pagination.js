26f36d61c986cedf68ab78de51fe79c442b76a63
Jean Nygård <jean@webbhuset.fi>
2016-09-30 15:58:06 +0300

    wip

---
+++ js/directive/pagination.js
@@ -0,0 +1,31 @@
+angular.module('RegistryClient')
+    .directive('xgPagination', function($exceptionHandler, $window, $log, dbHandler, dialogHandler) {
+        return {
+            restrict: 'E',
+            templateUrl: 'js/directive/template/pagination.html',
+            replace: true,
+            scope: {
+                limit: '=',
+                offset: '=',
+                count: '='
+            },
+            controller: function($scope) {
+                $scope.pages = {};
+                $scope.active = Number(Math.max($scope.offset/$scope.limit));
+                
+                var count = Math.max(Number($scope.count)/Number($scope.limit));
+                var i = 0;
+                while(i < count) {
+                    $scope.pages[i] = (Number(i)+1);
+                    if(i == count-1)
+                        $scope.last = Number(i);
+                    i++;
+                }
+                
+                $scope.pagination = function(id) {
+                    $scope.offset = $scope.limit * id;
+                    $scope.active = id;
+                }
+            }
+        }
+    })
