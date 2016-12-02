f013490e2463a2f27e625a21a9ad85ef8128efb1
Jean Nygård <jean@webbhuset.fi>
2016-12-02 14:45:46 +0200

    include '/' for login

--- js/controller/pageController.js
+++ js/controller/pageController.js
@@ -1,6 +1,6 @@
 angular.module('RegistryClient')
 .controller('pageController',  function ($scope, $location, $log) {
-    if($location.path().trim('/') == '/user/login')
+    if($location.path().trim('/') == '/user/login' || $location.path() == '/')
         $scope.layout = 'login';
     else
         $scope.layout = 'default';
