4b2e978014c6f90f63ffc1c7bf68c9bc2789bf27
Jean Nygård <jean@webbhuset.fi>
2016-12-23 12:07:48 +0200

    kick out users without a session

--- js/controller/navigation.js
+++ js/controller/navigation.js
@@ -1,6 +1,10 @@
 angular.module('RegistryClient')
-.controller('navigation', function($scope, $routeParams, $log, $window, dbHandler, globalParams){
-    $scope.user = globalParams.get('user');
-    $scope.globalParams = globalParams;
-    $scope.routeParams = $routeParams;
+.controller('navigation', function($scope, $routeParams, $log, $location, $window, dbHandler, globalParams){
+    if(globalParams.get('user')) {
+        $scope.user = globalParams.get('user');
+        $scope.globalParams = globalParams;
+        $scope.routeParams = $routeParams;
+    }else{
+        $location.path('/user/logout');
+    }
 });
